3bb891496eb633cbf94aa18cf951f02ddf3a0af8
Ester Morgenstern <em0527104104@gmail.com>
2025-03-30 18:29:13 +0300

    check design for login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/logIn'
+import AddItemDialog from './components/dialog'
 
 function App() {
 
   return (
     <>
-     <h1>Hi!!!!!!!!</h1>
+     <Login></Login>
+     {/* <AddItemDialog></AddItemDialog> */}
     </>
   )
 }
